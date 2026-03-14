147a9147d223e90de8cd85bd2ce0e397892781d6
吴天一 <faithleysath@gmail.com>
2026-03-14 23:19:51 +0800

    Refactor NapcatAdapter to extend BaseAdapter and include versioning properties

--- src/ncatbot/adapters/napcat/__init__.py
+++ src/ncatbot/adapters/napcat/__init__.py
@@ -1,7 +1,27 @@
-import sys
+import napcat.exceptions
+import napcat.matcher
+import napcat.types
+from napcat import NapCatClient, __version__
 
-import napcat
+from ncatbot.adapters import BaseAdapter
 
-sys.modules[__name__] = napcat
 
-NapcatAdapter = napcat.NapCatClient
+class NapcatAdapter(BaseAdapter, NapCatClient):
+    @property
+    def adapter_name(self) -> str:
+        return "Ncatbot.NapcatAdapter"
+
+    @property
+    def adapter_version(self) -> str:
+        return __version__
+
+    @property
+    def platform_name(self) -> str:
+        return "qq"
+
+
+exceptions = napcat.exceptions
+types = napcat.types
+matcher = napcat.matcher
+
+__all__ = ["NapcatAdapter", "exceptions", "types", "matcher"]
